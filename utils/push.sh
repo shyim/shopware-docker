5eb656d3c21dae36bfae8390b30b41a0f92203b0
Shyim <6224096+shyim@users.noreply.github.com>
2018-10-18 08:31:33 +0200

    Update push.sh

--- utils/push.sh
+++ utils/push.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 docker push shyim/shopware-cli:php72
+docker push shyim/shopware-nginx:php73
 docker push shyim/shopware-nginx:php72
 docker push shyim/shopware-nginx:php72-xdebug
 docker push shyim/shopware-nginx:php71
