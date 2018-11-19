a386c131dc26a212e58635fc67c7dac306c376ad
Soner Sayakci <s.sayakci@shopware.com>
2018-11-19 13:00:27 +0100

    Add php bcmath, fix rsnap, add bi-reset shortcut

---
+++ fixtures/bi-reset.sql
@@ -0,0 +1,13 @@
+UPDATE `s_benchmark_config` SET
+`shop_id` = '1',
+`last_sent` = '1970-11-14 13:56:31',
+`last_received` = '1970-01-01 00:00:00',
+`last_order_id` = '0',
+`last_customer_id` = '0',
+`last_product_id` = '0',
+`last_analytics_id` = '0',
+`last_updated_orders_date` = NULL,
+`response_token` = NULL,
+`cached_template` = NULL,
+`locked` = NULL
+WHERE active = 1
