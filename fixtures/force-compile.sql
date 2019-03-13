e5db1272d83a46e7513ee05ed693c23d1a848f8b
Soner Sayakci <s.sayakci@shopware.com>
2019-03-13 08:52:21 +0100

    Add force compile fixture

---
+++ fixtures/force-compile.sql
@@ -0,0 +1,6 @@
+UPDATE `s_core_theme_settings` SET
+`compiler_force` = '1',
+`compiler_create_source_map` = '0',
+`compiler_compress_css` = '0',
+`compiler_compress_js` = '0',
+`force_reload_snippets` = '0' 
