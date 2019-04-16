b8e7064bb692c07ebd4446bed1217e1b897e71f1
Soner Sayakci <s.sayakci@gmail.com>
2019-04-16 21:52:10 +0200

    Created a debug-logs command [ci skip]

---
+++ modules/base/debug-logs.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+echo "=== docker-compose.yml" > debug.txt
+echo "" >> debug.txt
+
+cat docker-compose.yml >> debug.txt
+
+echo "" >> debug.txt
+echo "" >> debug.txt
+
+echo "=== docker-compose.override.yaml" >> debug.txt
+echo "" >> debug.txt
+
+cat docker-compose.override.yaml >> debug.txt
+
+echo "" >> debug.txt
+echo "" >> debug.txt
+
+echo "=== .env" >> debug.txt
+echo "" >> debug.txt
+
+cat .env >> debug.txt
+
+echo "" >> debug.txt
+echo "" >> debug.txt
+
+echo "=== mysql logs" >> debug.txt
+echo "" >> debug.txt
+
+docker-compose logs mysql >> debug.txt
+
+echo "Generated a debug.txt file. Please post it on Github"
---
+++ modules/base/update-images.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker-compose pull
+docker-compose up -d --remove-orphans
