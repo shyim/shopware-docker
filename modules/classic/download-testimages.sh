c44dfb259c4d8aeb825485e2774f1cc72074a196
Shyim <6224096+shyim@users.noreply.github.com>
2019-05-14 10:14:15 +0200

    Update download-testimages.sh

--- modules/classic/download-testimages.sh
+++ modules/classic/download-testimages.sh
@@ -12,4 +12,4 @@ if [[ ! -f "${SNAP_DIR}/test_images.zip" ]]; then
     wget -O ${SNAP_DIR}/test_images.zip http://releases.s3.shopware.com/test_images_since_5.1.zip
 fi
 
-    unzip -o ${SNAP_DIR}/test_images.zip -d ${SHOPWARE_FOLDER}/
+unzip -o ${SNAP_DIR}/test_images.zip -d ${SHOPWARE_FOLDER}/
