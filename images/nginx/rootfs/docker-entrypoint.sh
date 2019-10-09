40ff836c9aea54c14d10a49c39edc806027e6cbd
Soner Sayakci <s.sayakci@shopware.com>
2019-10-09 11:55:27 +0200

    Fix websockets in mail, fix hostname on sw6 hosts

--- images/nginx/rootfs/docker-entrypoint.sh
+++ images/nginx/rootfs/docker-entrypoint.sh
@@ -3,14 +3,14 @@
 export PROXY_DOMAIN="mail"
 export PROXY_HOST="smtp"
 export PROXY_PORT="8025"
-cat /etc/nginx/proxy.tpl | envsubst > /etc/nginx/sites-enabled/mail.conf
+cat /etc/nginx/proxy.tpl | envsubst '\$PROXY_DOMAIN \$PROXY_HOST \$PROXY_PORT' > /etc/nginx/sites-enabled/mail.conf
 
 if [ "$DATABASE_TOOL" == "adminer" ]
 then
     export PROXY_DOMAIN="db"
     export PROXY_HOST="adminer"
     export PROXY_PORT="8080"
-    cat /etc/nginx/proxy.tpl | envsubst > /etc/nginx/sites-enabled/db.conf
+    cat /etc/nginx/proxy.tpl | envsubst '\$PROXY_DOMAIN \$PROXY_HOST \$PROXY_PORT' > /etc/nginx/sites-enabled/db.conf
 fi
 
 if [ "$DATABASE_TOOL" == "phpmyadmin" ]
@@ -18,7 +18,7 @@ then
     export PROXY_DOMAIN="db"
     export PROXY_HOST="phpmyadmin"
     export PROXY_PORT="80"
-    cat /etc/nginx/proxy.tpl | envsubst > /etc/nginx/sites-enabled/db.conf
+    cat /etc/nginx/proxy.tpl | envsubst '\$PROXY_DOMAIN \$PROXY_HOST \$PROXY_PORT' > /etc/nginx/sites-enabled/db.conf
 fi
 
 if [ "$ENABLE_ELASTICSEARCH" == "true" ]
@@ -26,12 +26,12 @@ then
     export PROXY_DOMAIN="cerebro"
     export PROXY_HOST="cerebro"
     export PROXY_PORT="9000"
-    cat /etc/nginx/proxy.tpl | envsubst > /etc/nginx/sites-enabled/cerebro.conf
+    cat /etc/nginx/proxy.tpl | envsubst '\$PROXY_DOMAIN \$PROXY_HOST \$PROXY_PORT' > /etc/nginx/sites-enabled/cerebro.conf
 
     export PROXY_DOMAIN="es"
     export PROXY_HOST="elastic"
     export PROXY_PORT="9200"
-    cat /etc/nginx/proxy.tpl | envsubst > /etc/nginx/sites-enabled/es.conf
+    cat /etc/nginx/proxy.tpl | envsubst '\$PROXY_DOMAIN \$PROXY_HOST \$PROXY_PORT' > /etc/nginx/sites-enabled/es.conf
 fi
 
 exec "$@"
