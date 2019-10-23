01553627cb052a619a6537c6f04fa715e3267387
Soner Sayakci <s.sayakci@shopware.com>
2019-10-23 11:47:34 +0200

    Fix admin build command

--- modules/platform/admin-build.sh
+++ modules/platform/admin-build.sh
@@ -1,10 +1,8 @@
 checkParameter
 
 cd "/var/www/html/${SHOPWARE_PROJECT}"
+export PROJECT_ROOT="/var/www/html/${SHOPWARE_PROJECT}"
 
 bin/console bundle:dump
-npm clean-install --prefix vendor/shopware/platform/src/Administration/Resources
-npm run --prefix vendor/shopware/platform/src/Administration/Resources lerna -- bootstrap
 npm run --prefix vendor/shopware/platform/src/Administration/Resources/administration/ build
-
 bin/console assets:install
