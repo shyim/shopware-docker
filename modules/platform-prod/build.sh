bbff1a03cfda8473ba2d2a0e01566ee9cb654822
tinect <s.koenig@tinect.de>
2020-07-17 12:43:35 +0200

    fix mailerUrl in platform

--- modules/platform-prod/build.sh
+++ modules/platform-prod/build.sh
@@ -13,7 +13,7 @@ INSTANCE_ID=$(openssl rand -hex 32)
 echo "APP_ENV=dev
 APP_SECRET=${SECRET}
 APP_URL=${URL}
-MAILER_URL=\"sendmail://localhost?command=ssmtp -t\"
+MAILER_URL=smtp://smtp:25
 INSTANCE_ID=${INSTANCE_ID}
 DATABASE_URL=mysql://root:${MYSQL_ROOT_PASSWORD}@mysql:3306/${SHOPWARE_PROJECT}
 SHOPWARE_ES_HOSTS=elastic
