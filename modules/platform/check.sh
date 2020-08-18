1247318c2db8853f8649fd0221458ca864b5a60d
Soner Sayakci <s.sayakci@shopware.com>
2020-08-18 08:59:56 +0200

    Fix check code command for SW6

--- modules/platform/check.sh
+++ modules/platform/check.sh
@@ -3,8 +3,9 @@ cd "/var/www/html/${SHOPWARE_PROJECT}"
 composer install -d dev-ops/analyze
 composer dump-autoload
 
+php dev-ops/analyze/generate-composer.php
 php dev-ops/analyze/vendor/bin/ecs check --fix platform/src --config platform/easy-coding-standard.yml
 
 php dev-ops/analyze/phpstan-config-generator.php
-php dev-ops/analyze/vendor/bin/phpstan analyze --configuration vendor/shopware/platform/phpstan.neon
-php dev-ops/analyze/vendor/bin/psalm --config=vendor/shopware/platform/psalm.xml --threads=4 --show-info=false
+php dev-ops/analyze/vendor/bin/phpstan analyze --autoload-file=dev-ops/analyze/vendor/autoload.php --configuration platform/phpstan.neon
+php dev-ops/analyze/vendor/bin/psalm --config=vendor/shopware/platform/psalm.xml --threads=$(nproc) --show-info=false
