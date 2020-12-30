39faf09cce6beac3448004f50ae7c33299c38b96
Soner Sayakci <s.sayakci@shopware.com>
2020-12-30 10:35:48 +0100

    Don't use all cores on psalm

--- modules/platform/check.sh
+++ modules/platform/check.sh
@@ -13,4 +13,4 @@ fi
 
 php dev-ops/analyze/phpstan-config-generator.php
 php dev-ops/analyze/vendor/bin/phpstan analyze --autoload-file=dev-ops/analyze/vendor/autoload.php --configuration platform/phpstan.neon
-php dev-ops/analyze/vendor/bin/psalm --config=vendor/shopware/platform/psalm.xml --threads=$(nproc) --show-info=false
+php dev-ops/analyze/vendor/bin/psalm --config=vendor/shopware/platform/psalm.xml --threads=$(($(nproc) / 2)) --show-info=false
