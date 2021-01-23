461837d55a5bab4500a7643bf983c0a4ae495dc8
Soner Sayakci <s.sayakci@shopware.com>
2021-01-23 12:14:29 +0100

    [ci skip] Fix drop command

--- modules/base/drop.sh
+++ modules/base/drop.sh
@@ -2,4 +2,10 @@
 
 checkParameter
 
-compose exec mysql mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "DROP DATABASE $SHOPWARE_PROJECT"
+CONTAINER=$3
+
+if [[ -z $CONTAINER ]]; then
+    CONTAINER="mysql"
+fi
+
+echo "DROP DATABASE $SHOPWARE_PROJECT" | compose exec -T mysql mysql -uroot -p${MYSQL_ROOT_PASSWORD}
