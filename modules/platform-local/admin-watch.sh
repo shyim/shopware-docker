95ff9a19e61599131aaa2589bf15aa42a6d39505
Soner Sayakci <s.sayakci@shopware.com>
2021-02-12 15:55:35 +0000

    [build skip] Fix errors from linter

--- modules/platform-local/admin-watch.sh
+++ modules/platform-local/admin-watch.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/var/www/html/${SHOPWARE_PROJECT}"
 LOCAL_PROJECT_ROOT="${CODE_DIRECTORY}/${SHOPWARE_PROJECT}"
-cd $LOCAL_PROJECT_ROOT
+cd "$LOCAL_PROJECT_ROOT" || exit
 PLATFORM_PATH=$(platform_component Administration)
 LOCAL_WEBPACK_CONFIG="${LOCAL_PROJECT_ROOT}/${PLATFORM_PATH}Resources/app/administration/webpack.config.js"
 export USE_SSL_DEFAULT=false
 URL=$(get_url "$SHOPWARE_PROJECT")
-HOST=$(echo $URL | sed s/'http[s]\?:\/\/'//)
+HOST=$(echo "$URL" | sed s/'http[s]\?:\/\/'//)
 WATCHER_URL="admin-${SHOPWARE_PROJECT}.${DEFAULT_DOMAIN}"
 
 if [[ -e "${LOCAL_WEBPACK_CONFIG}" ]]; then
     disabledHostCheck=$(cat ${LOCAL_WEBPACK_CONFIG} | grep disableHostCheck)
     
-    if [[ ! -z "$disabledHostCheck" ]]; then
+    if [[ -n "$disabledHostCheck" ]]; then
         docker exec -t -w "/var/www/html/${SHOPWARE_PROJECT}/" shopware-docker_cli_1 ./bin/console bundle:dump
         docker exec -t -w "/var/www/html/${SHOPWARE_PROJECT}/" shopware-docker_cli_1 ./bin/console feature:dump || true
 
@@ -23,14 +22,14 @@ if [[ -e "${LOCAL_WEBPACK_CONFIG}" ]]; then
             --name "${SHOPWARE_PROJECT}_admin_watch" \
             --network shopware-docker_default \
             --link "shopware-docker_app_${SHOPWARE_PROJECT}:${HOST}" \
-            -v $LOCAL_PROJECT_ROOT:/var/www/html \
+            -v "$LOCAL_PROJECT_ROOT:/var/www/html" \
             -e PORT=80 \
             -e HOST="0.0.0.0" \
             -e ESLINT_DISABLE=true \
-            -e APP_URL=$URL \
+            -e "APP_URL=$URL" \
             -e PROJECT_ROOT="/var/www/html" \
             -e ENV_FILE="/var/www/html/.env" \
-            -e VIRTUAL_HOST=$WATCHER_URL \
+            -e "VIRTUAL_HOST=$WATCHER_URL" \
             -w "/var/www/html" \
             --expose 80 \
             node:12-alpine \
