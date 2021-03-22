c237c4302e8d8cffd0f5bb37e84df483438a6ccf
Soner Sayakci <s.sayakci@shopware.com>
2021-03-22 16:13:02 +0000

    [build skip] Ignore shell check errors

--- completion.sh
+++ completion.sh
@@ -1,14 +1,13 @@
-FILE="$0"
+#!/usr/bin/env bash
 
-if [[ -L "$0" ]]; then
-  FILE="$(readlink "${0}")"
-fi
-
+# shellcheck disable=SC2128
 DIR="$(dirname "${BASH_SOURCE}")"
 CONFIG_DIR=${XDG_CONFIG_HOME:-${HOME}/.config}
 
 # shellcheck source=.env.dist
 source "${CONFIG_DIR}/swdc/env"
+
+# shellcheck disable=SC2086
 AVAILABLE_CMDS=$(find $DIR/modules/* -maxdepth 1 -mindepth 1 -iname "*.sh")
 
 function __list_swdc_commands {
@@ -17,31 +16,38 @@ function __list_swdc_commands {
     if [[ "$3" == "swdc" ]]; then
         local cmds
         for file in $AVAILABLE_CMDS; do
-            file=$(basename $file)
+            file=$(basename "$file")
             file=${file%.*}
 
             cmds="$cmds $file"
         done
 
+        # shellcheck disable=SC2207
         COMPREPLY=($(compgen -W "$cmds" "${COMP_WORDS[1]}"))
     elif [[ "$AVAILABLE_CMDS" == *"$3"* ]]; then
         local cmds
-        for file in $(find $CODE_DIRECTORY/ -maxdepth 1 -mindepth 1); do
-            file=$(basename $file)
+
+        # shellcheck disable=SC2044
+        for file in $(find "$CODE_DIRECTORY/" -maxdepth 1 -mindepth 1); do
+            file=$(basename "$file")
 
             cmds="$cmds $file"
         done
 
+        # shellcheck disable=SC2207
         COMPREPLY=($(compgen -W "$cmds" "${COMP_WORDS[2]}"))
     elif [[ "$prev_arg" == "snap" || "$prev_arg" == "rsnap" ]]; then
         local cmds
-        for file in $(find $CODE_DIRECTORY/snapshots/ -maxdepth 1 -mindepth 1 -iname "$3*.sql"); do
-            file=$(basename $file | cut -d '-' -f 2)
+
+        # shellcheck disable=SC2044
+        for file in $(find "$CODE_DIRECTORY/snapshots/" -maxdepth 1 -mindepth 1 -iname "$3*.sql"); do
+            file=$(basename "$file" | cut -d '-' -f 2)
             file=${file%.*}
 
             cmds="$cmds $file"
         done
 
+        # shellcheck disable=SC2207
         COMPREPLY=($(compgen -W "$cmds" "${COMP_WORDS[3]}"))
     fi
 
