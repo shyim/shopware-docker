cca77cff745429cb8e40c9d2c30f62ac06db8c6c
Soner Sayakci <s.sayakci@shopware.com>
2021-03-22 16:02:48 +0000

    [build skip] Add basic bash completion

---
+++ completion.sh
@@ -0,0 +1,51 @@
+FILE="$0"
+
+if [[ -L "$0" ]]; then
+  FILE="$(readlink "${0}")"
+fi
+
+DIR="$(dirname "${BASH_SOURCE}")"
+CONFIG_DIR=${XDG_CONFIG_HOME:-${HOME}/.config}
+
+# shellcheck source=.env.dist
+source "${CONFIG_DIR}/swdc/env"
+AVAILABLE_CMDS=$(find $DIR/modules/* -maxdepth 1 -mindepth 1 -iname "*.sh")
+
+function __list_swdc_commands {
+    prev_arg="${COMP_WORDS[COMP_CWORD-2]}";
+
+    if [[ "$3" == "swdc" ]]; then
+        local cmds
+        for file in $AVAILABLE_CMDS; do
+            file=$(basename $file)
+            file=${file%.*}
+
+            cmds="$cmds $file"
+        done
+
+        COMPREPLY=($(compgen -W "$cmds" "${COMP_WORDS[1]}"))
+    elif [[ "$AVAILABLE_CMDS" == *"$3"* ]]; then
+        local cmds
+        for file in $(find $CODE_DIRECTORY/ -maxdepth 1 -mindepth 1); do
+            file=$(basename $file)
+
+            cmds="$cmds $file"
+        done
+
+        COMPREPLY=($(compgen -W "$cmds" "${COMP_WORDS[2]}"))
+    elif [[ "$prev_arg" == "snap" || "$prev_arg" == "rsnap" ]]; then
+        local cmds
+        for file in $(find $CODE_DIRECTORY/snapshots/ -maxdepth 1 -mindepth 1 -iname "$3*.sql"); do
+            file=$(basename $file | cut -d '-' -f 2)
+            file=${file%.*}
+
+            cmds="$cmds $file"
+        done
+
+        COMPREPLY=($(compgen -W "$cmds" "${COMP_WORDS[3]}"))
+    fi
+
+    return 0;
+}
+
+complete -F __list_swdc_commands swdc
