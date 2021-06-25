ecba0a4e9b0b16dd631deb492da3771877ae87f2
Soner Sayakci <s.sayakci@shopware.com>
2021-06-25 07:02:38 +0000

    fix: Add info when node_modules does not exists on admin build [build skip]

--- modules/platform/admin-build.sh
+++ modules/platform/admin-build.sh
@@ -11,6 +11,12 @@ bin/console feature:dump || true
 
 PLATFORM_PATH=$(platform_component Administration)
 
+if [[ ! -f "${PLATFORM_PATH}/Resources/app/administration/" ]]; then
+  echo "Build failed:"
+  echo "=> Run swdc admin-init ${SHOPWARE_PROJECT} first."
+  exit 1
+fi
+
 npm run --prefix "$PLATFORM_PATH"/Resources/app/administration/ build
 
 bin/console assets:install
