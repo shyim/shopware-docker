1cfc48b85ede40796cb5735834d06a8ac86d1871
Soner Sayakci <s.sayakci@shopware.com>
2021-08-02 13:12:07 +0200

    fix: Make test run more portable [ci skip]

--- modules/platform/test.sh
+++ modules/platform/test.sh
@@ -8,8 +8,14 @@ export PROJECT_ROOT=$SHOPWARE_FOLDER
 
 composer dump-autoload
 
+PHPUNIT_XML=vendor/shopware/platform/phpunit.xml.dist
+
+if [[ -e phpunit.xml.dist ]]; then
+  PHPUNIT_XML=phpunit.xml.dist
+fi
+
 if [[ $3 == "--with-coverage" ]]; then
-  php -d memory_limit=-1 -d pcov.enabled=1 -d pcov.directory=/var/www/html/"${SHOPWARE_PROJECT}" vendor/bin/phpunit -c vendor/shopware/platform/phpunit.xml.dist --coverage-html build/artifacts/phpunit-coverage-html "${@:4}"
+  php -d memory_limit=-1 -d pcov.enabled=1 -d pcov.directory=/var/www/html/"${SHOPWARE_PROJECT}" vendor/bin/phpunit -c ${PHPUNIT_XML} --coverage-html build/artifacts/phpunit-coverage-html "${@:4}"
 else
-  php -d memory_limit=-1 vendor/bin/phpunit -c vendor/shopware/platform/phpunit.xml.dist "${@:3}"
+  php -d memory_limit=-1 vendor/bin/phpunit -c ${PHPUNIT_XML} "${@:3}"
 fi
