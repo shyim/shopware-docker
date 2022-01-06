b6f3f3589a0a4c06b2c7572840fde62b7a590831
Shyim <6224096+shyim@users.noreply.github.com>
2022-01-06 09:21:29 +0100

    Fix codestyle issues

--- modules/platform-b2b/b2b-init.sh
+++ modules/platform-b2b/b2b-init.sh
@@ -18,6 +18,7 @@ cp -rf "${B2B_ROOT}/dev-ops/package/templates/composer.json" "${B2B_ROOT}/compos
 B2B_COMPOSER="${B2B_ROOT}/composer.json"
 PLATFORM_COMPOSER="${PROJECT_ROOT}/composer.json"
 
+
 if [[ "$(jq 'has("repositories")' "$PLATFORM_COMPOSER")" == "true" ]]; then
   if [[ "$(jq '.repositories | any(.url != "custom/b2b")' "$PLATFORM_COMPOSER")" == "true" ]]; then
     echo $(jq '.repositories[.repositories | length] |= . + {"type":"path", "url": "custom/b2b"}' "$PLATFORM_COMPOSER") > "$PLATFORM_COMPOSER"
@@ -26,9 +27,12 @@ else
   echo $(jq '.repositories = [{"type":"path", "url": "custom/b2b"}]' "$PLATFORM_COMPOSER") > "$PLATFORM_COMPOSER"
 fi
 
+php -r "file_put_contents(\"${PLATFORM_COMPOSER}\", json_encode(json_decode(file_get_contents(\"${PLATFORM_COMPOSER}\"), true), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));"
+php -r "file_put_contents(\"${B2B_COMPOSER}\", json_encode(json_decode(file_get_contents(\"${B2B_COMPOSER}\"), true), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));"
+
 B2B_VERSION=$(php "${B2B_ROOT}/dev-ops/package/version.php")
 
-echo $(jq --arg v $B2B_VERSION '.version = $v | .autoload["psr-4"]["SwagB2bPlatform\\"] = "components/SwagB2bPlatform"' $B2B_COMPOSER) > $B2B_COMPOSER
+echo $(jq --arg v "$B2B_VERSION" '.version = $v | .autoload["psr-4"]["SwagB2bPlatform\\"] = "components/SwagB2bPlatform"' "$B2B_COMPOSER") > "$B2B_COMPOSER"
 
 npm clean-install --prefix "${B2B_ROOT}/components/SwagB2bPlatform/Resources/app/storefront/"
 
