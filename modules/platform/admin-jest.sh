e450383c7517e1f4e4fbc7759944c1399d61cbc6
Shyim <6224096+shyim@users.noreply.github.com>
2022-04-12 11:35:02 +0200

    run entity schema dump before running jest

--- modules/platform/admin-jest.sh
+++ modules/platform/admin-jest.sh
@@ -7,4 +7,8 @@ export PROJECT_ROOT="/var/www/html/${SHOPWARE_PROJECT}"
 export ENV_FILE="${PROJECT_ROOT}/.env"
 ADMINISTRATION_PATH="${PROJECT_ROOT}/$(platform_component Administration)"
 
+bin/console framework:schema -s 'entity-schema' "${ADMINISTRATION_PATH}Resources/app/administration/test/_mocks_/entity-schema.json"
+
+setup_node_version
+
 npm run --prefix "${ADMINISTRATION_PATH}Resources/app/administration" unit-watch
