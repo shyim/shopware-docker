96bc72566cefcef5d236770f2f46389ad67f1ea7
Soner Sayakci <s.sayakci@shopware.com>
2022-05-05 18:04:43 +0000

    fixL admin-init on old versions

--- modules/platform/admin-init.sh
+++ modules/platform/admin-init.sh
@@ -9,7 +9,7 @@ setup_node_version
 ADMINISTRATION_PATH=$(platform_component Administration)
 
 if [[ -e "$ADMINISTRATION_PATH/Resources/lerna.json" ]]; then
-  npm clean-install --prefix "$ADMINISTRATION_PATH/Resources"
+  npm install --prefix "$ADMINISTRATION_PATH/Resources"
   npm run --prefix "$ADMINISTRATION_PATH/Resources" lerna -- bootstrap
 else
   npm clean-install --prefix "$ADMINISTRATION_PATH/Resources/app/administration/"
