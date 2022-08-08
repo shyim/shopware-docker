4440915c1b8181dd8543ef58d854e2fb767ede4c
Dominik <mail@dominikk.me>
2022-08-08 11:03:41 +0200

    Add plugin e2e

--- modules/platform-local/e2e.sh
+++ modules/platform-local/e2e.sh
@@ -2,6 +2,7 @@
 
 SHOPWARE_PROJECT=$2
 MODULE=$3
+PLUGIN=$4
 export USE_SSL_DEFAULT=false
 URL=$(get_url "$SHOPWARE_PROJECT")
 
@@ -11,11 +12,28 @@ fi
 
 cd "${CODE_DIRECTORY}/${SHOPWARE_PROJECT}" || exit
 
-E2E_DIR="tests/e2e"
-E2E_PATH="/var/www/html/${SHOPWARE_PROJECT}/${E2E_DIR}"
+if [[ -z $PLUGIN ]]; then
+  E2E_PATH="${SHOPWARE_PROJECT}/tests/e2e"
+  CYPRESS_BIN="/var/www/html/${SHOPWARE_PROJECT}/tests/e2e/node_modules/.bin/cypress"
+else
+  PLUGIN_MODULE=$(if [ $MODULE = "Administration" ]; then echo "administration"; else echo "storefront"; fi)
 
-if [[ ! -d "${E2E_DIR}/node_modules" ]]; then
-    npm install --prefix "${E2E_DIR}"
+  E2E_PATH="${SHOPWARE_PROJECT}/custom/plugins/${PLUGIN}/src/Resources/app/${PLUGIN_MODULE}/test/e2e"
+  CYPRESS_BIN="/var/www/html/${E2E_PATH}/node_modules/.bin/cypress"
+
+  compose exec -w "/var/www/html/${SHOPWARE_PROJECT}" cli php bin/console plugin:refresh
+
+  PLUGIN_ACTIVE=$(echo "SELECT active FROM plugin WHERE name = '${PLUGIN}' LIMIT 1" | compose exec -T mysql mysql -uroot -proot "$SHOPWARE_PROJECT" | grep -E -i '^[0-9]+$')
+
+  if [[ $PLUGIN_ACTIVE -ne 1 ]]; then
+    compose exec -w "/var/www/html/${SHOPWARE_PROJECT}" cli php bin/console plugin:install -a "$PLUGIN"
+  fi
+fi
+
+REMOTE_E2E_PATH="/var/www/html/${E2E_PATH}"
+
+if [[ ! -d "${CODE_DIRECTORY}/${E2E_PATH}/node_modules" ]]; then
+  compose exec cli bash /opt/swdc/swdc-inside run "$2" npm install --prefix "$REMOTE_E2E_PATH"
 fi
 
 usedCypressVersion=$(docker run \
@@ -24,11 +42,11 @@ usedCypressVersion=$(docker run \
     -v "${CODE_DIRECTORY}:/var/www/html" \
     -u 1000 \
     node:12-alpine \
-    node "/var/www/html/${SHOPWARE_PROJECT}/tests/e2e/node_modules/.bin/cypress" --version | grep 'version: ' | head -1 | cut -d':' -f2)
+    node "$CYPRESS_BIN" --version | grep 'version: ' | head -1 | cut -d':' -f2)
 
 usedCypressVersion=$(trim_whitespace "$usedCypressVersion")
 
-xhost +si:localuser:root
+xhost +si:localuser:"$USER"
 HOST=$(echo "$URL" | sed s/'http[s]\?:\/\/'//)
 CURLHOST="Host: ${HOST}"
 
@@ -41,7 +59,7 @@ docker run \
       --network shopware-docker_default \
       -v "${CODE_DIRECTORY}/${SHOPWARE_PROJECT}:/var/www/html/${SHOPWARE_PROJECT}" \
       -v /tmp/.X11-unix:/tmp/.X11-unix \
-      -w "${E2E_PATH}" \
+      -w "$REMOTE_E2E_PATH" \
       -e DISPLAY \
       -e TZ=UTC \
       -e "CYPRESS_shopwareRoot=/var/www/html/${SHOPWARE_PROJECT}" \
